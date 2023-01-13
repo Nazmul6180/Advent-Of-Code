7536be692a5b8f9b5523af2d5e1eef585aa523e8
Nazmul <nazmul.hasan1.61803@gmail.com>
2023-01-13 18:14:38 +0600

    AOC Day-25 solu. JavaScript

--- Advent-Of-Code-2022/Day-25/puzzle-1.js
+++ Advent-Of-Code-2022/Day-25/puzzle-1.js
@@ -1,69 +1,40 @@
-const { notes } = require("./parse");
-const { map, moves } = notes;
-console.time("ExecutionTime");
+const fs = require("fs");
+const data = fs.readFileSync(`./input.txt`, "utf-8");
+const inputs = data.split("\r\n");
 
-const findWrapPosition = (pos, dir) => {
-  let oppositeDir = (dir + 2) % 4;
-  let { x, y } = pos;
+let sum = 0;
 
-  while (map[y]?.[x] !== undefined && map[y][x] !== " ") {
-    if (oppositeDir === 0) x++;
-    else if (oppositeDir === 1) y++;
-    else if (oppositeDir === 2) x--;
-    else if (oppositeDir === 3) y--;
-  }
-
-  if (oppositeDir === 0) x--;
-  else if (oppositeDir === 1) y--;
-  else if (oppositeDir === 2) x++;
-  else if (oppositeDir === 3) y++;
-
-  return { x, y };
-};
-
-const findNextPos = (move, pos, dir) => {
-  for (let i = 0; i < move; i++) {
-    let nextPos = { x: pos.x, y: pos.y };
-    if (dir === 0) nextPos.x++;
-    else if (dir === 1) nextPos.y++;
-    else if (dir === 2) nextPos.x--;
-    else if (dir === 3) nextPos.y--;
-
-    if (
-      map[nextPos.y]?.[nextPos.x] === undefined ||
-      map[nextPos.y][nextPos.x] === " "
-    ) {
-      let wrapPos = findWrapPosition(pos, dir);
-      console.log("called", pos);
-      if (map[wrapPos.y][wrapPos.x] === "#") return pos;
-      nextPos.x = wrapPos.x;
-      nextPos.y = wrapPos.y;
-    }
-
-    if (map[nextPos.y][nextPos.x] === "#") return pos;
-
-    pos.x = nextPos.x;
-    pos.y = nextPos.y;
-  }
-
-  return pos;
-};
-
-let dir = 0;
-let pos = { x: map[0].indexOf("."), y: 0 };
-
-while (moves.length > 0) {
-  let move = moves.shift();
-
-  if (move === "L") dir = (dir + 3) % 4;
-  else if (move === "R") dir = (dir + 1) % 4;
-  else {
-    pos = findNextPos(move, pos, dir);
-  }
+for(const line of inputs) {
+    sum += snafuToDec(line);
 }
 
-let password = (pos.y + 1) * 1000 + (pos.x + 1) * 4 + dir;
+console.log(sum);
+console.log(decToSnafu(sum));
 
-console.log(password);
+function snafuToDec(s) {
+    const snafuTable = {"=": -2, "-": -1, "0": 0, "1": 1, "2": 2};
+    let len = s.length;
+    let d = 0;
+    for(const c of s) {
+        d += snafuTable[c] * Math.pow(5, --len);
+    }
+    return d;
+}
 
-console.timeEnd("ExecutionTime");
+function decToSnafu(dec) {
+    const decTable = {0: 0, 1: 1, 2: 2, 3: "=", 4: "-"};
+    let snafu = [];
+    while(dec > 0) {
+        let r = dec % 5;
+        snafu.unshift(decTable[r]);
+        if(r === 3){
+            dec += 2;
+        }
+        else if(r === 4) {
+            dec++;
+        }
+        dec = Math.floor(dec / 5);
+    }
+    return snafu.join("");
+}
+Footer
